b611ba3ad1ec8628cb646402978e6526de003e2f
Andrew Molnar <34353520+amolnar68@users.noreply.github.com>
2017-12-22 13:30:11 -0600

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -2,6 +2,7 @@ node default {
   file {'/root/README':
     ensure => file,
     content => "\nHey!  Here is some mind-blowing content!\n\n\n",
+    content => "\nAin't this the best?  More text added...\n\n\n",
     owner => root,
   }
 }
